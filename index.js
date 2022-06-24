23ac47456c64a443f032e2d238ebd747529217a4
AyoOlu1 <oluwayemivictor15@gmail.com>
2022-06-24 13:15:10 +0100

    print variables

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var name = "Victor Oluwayemi";
+var myname = "Victor Oluwayemi";
 var height = "I don't know yet";
 var country = "Nigeria";
+
+console.log(`Name: ${myname}`);
+console.log(`Height: ${height}`);
+console.log(`Country: ${country}`);
